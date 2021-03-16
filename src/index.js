6f5bf80bcd4e920ebe8e03801439216e9f8b2067
denchik3192 <den465341@gmail.com>
2021-03-16 12:25:27 +0300

    feat: towel sort algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+if(matrix === undefined || matrix.length < 0 ){
+  return[];
+}
+
+const sortedArr = matrix.reduce((total, amount, index) => {
+  if(index % 2 !== 0 ) matrix[index].reverse();
+  return total.concat(amount);
+}, []);
+
+  return sortedArr;
 }
